fd087b845f0d35ead88f04777369e9c0801c88ea
Nassim Brahimi <nassim.brahimi4@gmail.com>
2022-07-26 18:45:05 +0200

    Yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   # GET /restaurants/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "078986589", category: "Chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "5875756589", category: "Viet"}
+luigia =  { name: "Luigia", address: "31 Rue Marchaut, Essex E1 6PQ", phone_number: "23351341", category: "Italian"}
+chevrier =  { name: "Chateauvieux", address: "34 duffering street, London E3 QP2", phone_number: "235567686", category: "French"}
+chez_monique =  { name: "Chez Monique", address: "34 ering street, Paris E3 QP2", phone_number: "794664646", category: "French"}
+
+[ dishoom, pizza_east, luigia, chevrier, chez_monique ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
